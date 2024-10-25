ff7cc491ed59b41e4a2e3ab4e35dfc3145f73524
3thernitydev <3thernitydev@gmail.com>
2024-10-25 15:35:15 +0400

    usearBoard main content add

---
+++ website/userBoard/userBoard.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+include "../database/bdd.php";
+
+if (!isset($_SESSION['username']) || !isset($_SESSION['user_id']) || !isset($_SESSION['admin'])) {
+    header("Location: ../loginPage/loginPage.php");
+    exit();
+}
+
+function logOut()
+{
+    session_destroy();
+    header("Location: ../loginPage/loginPage.php"); // Redirect after logout
+    exit();
+}
+
+// Check if the logout request is made
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
+    logOut();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/userBoard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>GameHub | Profil</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="../images/logoGameHub.webp" alt="Logo de GameHub">
+            </div>
+            <div class="userInfo">
+                <p><?php echo htmlspecialchars($_SESSION['username']); ?></p>
+                <form method="post" style="display:inline;">
+                    <button type="submit" name="logout" style="background:none; border:none; cursor:pointer;">
+                        <i class="fa-solid fa-door-open"></i>
+                    </button>
+                </form>
+            </div>
+        </nav>
+    </header>
+
+    <div class="profilContent">
+        <h1>Bienvenue sur ton profil !</h1>
+        <div class="cardContainer">
+
+        </div>
+    </div>
+</body>
+
+</html>
