152de58dbd92c644bbf07afd6328d3408e4d7671
3thernitydev <3thernitydev@gmail.com>
2024-10-25 07:32:03 +0400

    add titlte of the project and logo

--- website/index.php
+++ website/index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>GameHub | Le réseau des gamers passionnés.</title>
 </head>
+
 <body>
-    
+    <nav>
+
+    </nav>
 </body>
+
 </html>
