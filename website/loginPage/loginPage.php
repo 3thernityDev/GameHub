54e9392fb1d5e103902e023115cb5f0e583e9142
3thernitydev <3thernitydev@gmail.com>
2024-10-25 09:26:47 +0400

    add login page and style

---
+++ website/loginPage/loginPage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/loginPage.css">
+    <title>GameHub | Ce connecter</title>
+</head>
+<header>
+    <nav>
+        <div class="logo">
+            <img src="../images/logoGameHub.webp" alt="Logo de GameHub">
+        </div>
+        <div class="logOPT">
+            <button class="cancel">Annuler</button>
+        </div>
+    </nav>
+</header>
+
+<body>
+    <div class="formContainer">
+        <form action="">
+
+        </form>
+    </div>
+</body>
+
+</html>
