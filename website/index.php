ee61e6bb2d151d80259a4b1185d20b85c994f7c5
3thernitydev <3thernitydev@gmail.com>
2024-10-25 07:49:01 +0400

    add navbar, title

--- website/index.php
+++ website/index.php
@@ -4,13 +4,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/index.css">
     <title>GameHub | Le réseau des gamers passionnés.</title>
 </head>
 
-<body>
+<header>
     <nav>
-
+        <div class="logo">
+            <img src="./images/logoGameHub.webp" alt="Logo de GameHub">
+        </div>
+        <div class="logOPT">
+            <button class="logIN">Connexion</button>
+            <button class="register">Inscription</button>
+        </div>
     </nav>
+</header>
+
+<body>
+    <div class="title">
+
+        <h1>GameHub</h1>
+        <p>Le réseau des gamers passsionnés.</p>
+    </div>
 </body>
 
 </html>
